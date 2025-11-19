bf76780a139a65f36076973cd49c325a1a1ebb8a
Terekhov Artur <terehov.rtr@gmail.com>
2025-11-19 16:39:06 +0300

    added config in TestBase.java

--- src/test/java/tests/simple/TestBase.java
+++ src/test/java/tests/simple/TestBase.java
@@ -3,14 +3,11 @@ package tests.simple;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
-
-import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
 
@@ -21,7 +18,7 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.timeout = 10000;
-        Configuration.remote = remoteAddress;
+//        Configuration.remote = remoteAddress;
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserVersion = System.getProperty("browserVersion");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
@@ -36,16 +33,6 @@ public class TestBase {
     @BeforeEach
     void addListener() {
         SelenideLogger.addListener("allureListener", new AllureSelenide());
-    }
-
-    @AfterEach
-    void addAttachments() {
-        Attach.screenshotAs("Last screenshot");
-        Attach.pageSource();
-        Attach.browserConsoleLogs();
-        Attach.addVideo();
-
-        closeWebDriver();
     }
 
 }
