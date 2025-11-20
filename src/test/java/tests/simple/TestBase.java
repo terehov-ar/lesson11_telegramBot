4477c54c8fd696536823a4859b71cf3ee74d0ef5
Terekhov Artur <terehov.rtr@gmail.com>
2025-11-20 09:42:25 +0300

    delete setupEnvironment

--- src/test/java/tests/simple/TestBase.java
+++ src/test/java/tests/simple/TestBase.java
@@ -1,31 +1,10 @@
 package tests.simple;
 
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-import java.util.Map;
 
 public class TestBase {
-
-    @BeforeAll
-    static void setupEnvironment() {
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.pageLoadStrategy = "eager";
-        Configuration.timeout = 10000;
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browserVersion");
-        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
-        Configuration.browserCapabilities = capabilities;
-    }
 
     @BeforeEach
     void addListener() {
